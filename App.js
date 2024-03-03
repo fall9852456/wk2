425fd37ef11b2c22056ab9f3831db913882902a7
fall9852456 <42578361+fall9852456@users.noreply.github.com>
2024-03-03 20:57:56 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, StatusBar, SafeAreaView } from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Header />
+      <AlbumList />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"#C06060",
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = () => {
+  return (
+      <View style={styles.headerStyle}>
+        <Text style={styles.textStyle}>Albums</Text>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor:"#CC6666"	,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    // Android Only
+    elevation: 4
+  },
+  textStyle: {
+    fontSize: 20,
+    color:"#FFFFFF"
+  },
+});
+
+export default Header;
